32b8ba562fadf8c49209266efc38cd921d6f4b21
theprajapat~ <himanshukajra2005@gmail.com~>
2024-11-04 16:14:05 +0530

    last commit

--- lab_exam/src/App.js
+++ lab_exam/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Routes, Router, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={Home} />
+          <Route path="/signup" element={Signup} />
+          <Route Path="/login" element={Login}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
-
 export default App;
---
+++ lab_exam/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); 
+const app = express();
+app.use(express.json());
+app.use(cors());
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/', {
+ useNewUrlParser: true, useUnifiedTopology: true, }).then(() => { })
+ .catch(err => console.log('MongoDB connection error:', err));
+// Routes
+app.use('/api/users', );
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
